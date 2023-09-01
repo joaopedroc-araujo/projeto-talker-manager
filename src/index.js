093c3c3727d9a13fbc620b1c5ef021ec3570d812
costaaraujojoaopedro@gmail.com <costaaraujojoaopedro@gmail.com>
2023-09-01 14:45:57 -0300

    Cria rota "/talker"

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const fs = require('fs').promises;
 const express = require('express');
 
 const app = express();
@@ -10,6 +11,12 @@ const PORT = process.env.PORT || '3001';
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+
+app.get('/talker', async (_req, res) => {
+  const data = await fs.readFile('./src/talker.json', 'utf8');
+  const talker = JSON.parse(data);
+  res.status(HTTP_OK_STATUS).json(talker || []);
+  });
 
 app.listen(PORT, () => {
   console.log('Online');
