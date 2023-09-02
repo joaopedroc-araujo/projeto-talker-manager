fcacb5ea07af9fd82c741f22bafdd20a4b829916
costaaraujojoaopedro@gmail.com <costaaraujojoaopedro@gmail.com>
2023-09-02 12:22:36 -0300

    Refatora index, coloca rotas no talkerRouter

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-const fs = require('fs').promises;
 const express = require('express');
 const loginRouter = require('./routers/loginRouter.router');
 const talkerRouter = require('./routers/talkerRouter.router');
@@ -14,23 +13,6 @@ app.use('/talker', talkerRouter);
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
-});
-
-app.get('/talker', async (_req, res) => {
-  const data = await fs.readFile('./src/talker.json', 'utf8');
-  const talker = JSON.parse(data);
-  res.status(HTTP_OK_STATUS).json(talker || []);
-});
-
-app.get('/talker/:id', async (req, res) => {
-  const { id } = req.params;
-  const data = await fs.readFile('./src/talker.json', 'utf8');
-  const talker = JSON.parse(data);
-  const foundTalker = talker.find((talk) => talk.id === parseInt(id, 10));
-  if (!foundTalker) {
-    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
-  }
-  res.status(HTTP_OK_STATUS).json(foundTalker);
 });
 
 app.listen(PORT, () => {
--- src/routers/talkerRouter.router.js
+++ src/routers/talkerRouter.router.js
@@ -4,11 +4,19 @@ const path = require('path');
 const validateToken = require('../middlewares/validateToken');
 const validateTalker = require('../middlewares/validateTalker');
 
-const router = express.Router();
-router.use(validateToken);
-router.use(validateTalker);
+const HTTP_OK_STATUS = 200;
 
-router.post('/', async (req, res) => {
+const router = express.Router();
+// router.use(validateToken);
+// router.use(validateTalker);
+
+router.get('/', async (_req, res) => {
+    const data = await fs.readFile('./src/talker.json', 'utf8');
+    const talker = JSON.parse(data);
+    res.status(HTTP_OK_STATUS).json(talker || []);
+});
+
+router.post('/', validateToken, validateTalker, async (req, res) => {
     const talker = req.body;
     try {
         const talkerFilePath = path.join(__dirname, '../talker.json');
@@ -21,11 +29,11 @@ router.post('/', async (req, res) => {
         // console.log([talker]);
         res.status(201).json(talker);
     } catch (err) {
-        res.status(400).json({ message: req.message });
+        res.status(401).json({ message: req.message });
     }
 });
 
-router.put('/:id', async (req, res) => {
+router.put('/:id', validateToken, validateTalker, async (req, res) => {
     const { id } = req.params;
     const talker = req.body;
     try {
@@ -41,6 +49,34 @@ router.put('/:id', async (req, res) => {
         const updatedData = JSON.stringify(talkers);
         await fs.writeFile(talkerFilePath, updatedData);
         res.status(200).json(talker);
+    } catch (err) {
+        res.status(400).json({ message: req.message });
+    }
+});
+
+router.get('/talker/:id', async (req, res) => {
+    const { id } = req.params;
+    const data = await fs.readFile('./src/talker.json', 'utf8');
+    const talker = JSON.parse(data);
+    const foundTalker = talker.find((talk) => talk.id === parseInt(id, 10));
+    if (!foundTalker) {
+        return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    }
+    res.status(HTTP_OK_STATUS).json(foundTalker);
+});
+
+router.delete('/:id', validateToken, validateTalker, async (req, res) => {
+    const { id } = req.params;
+    try {
+        const talkerFilePath = path.join(__dirname, '../talker.json');
+        const data = await fs.readFile(talkerFilePath, 'utf8');
+        const talkers = JSON.parse(data);
+        const talkerIndex = talkers.findIndex((person) => person.id === Number(id));
+        talkers.splice(talkerIndex, 1);
+        const updatedData = JSON.stringify(talkers);
+        // console.log(updatedData);
+        await fs.writeFile(talkerFilePath, updatedData);
+        res.status(204).json({ message: 'Pessoa palestrante deletada com sucesso' });
     } catch (err) {
         res.status(400).json({ message: req.message });
     }
