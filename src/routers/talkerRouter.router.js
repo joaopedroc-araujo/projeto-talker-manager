6ae6998a9d3c8569762fd46b6dab0dd414beb3e6
costaaraujojoaopedro@gmail.com <costaaraujojoaopedro@gmail.com>
2023-09-01 23:26:58 -0300

    Cria rota PUT do talkers/:id

--- src/routers/talkerRouter.router.js
+++ src/routers/talkerRouter.router.js
@@ -5,8 +5,10 @@ const validateToken = require('../middlewares/validateToken');
 const validateTalker = require('../middlewares/validateTalker');
 
 const router = express.Router();
+router.use(validateToken);
+router.use(validateTalker);
 
-router.post('/', validateToken, validateTalker, async (req, res) => {
+router.post('/', async (req, res) => {
     const talker = req.body;
     try {
         const talkerFilePath = path.join(__dirname, '../talker.json');
@@ -18,6 +20,27 @@ router.post('/', validateToken, validateTalker, async (req, res) => {
         await fs.writeFile(talkerFilePath, updatedData);
         // console.log([talker]);
         res.status(201).json(talker);
+    } catch (err) {
+        res.status(400).json({ message: req.message });
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    const talker = req.body;
+    try {
+        const talkerFilePath = path.join(__dirname, '../talker.json');
+        const data = await fs.readFile(talkerFilePath, 'utf8');
+        const talkers = JSON.parse(data);
+        const talkerIndex = talkers.findIndex((person) => person.id === Number(id));
+        if (talkerIndex === -1) {
+            return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+        }
+        talker.id = Number(id);
+        talkers[talkerIndex] = talker;
+        const updatedData = JSON.stringify(talkers);
+        await fs.writeFile(talkerFilePath, updatedData);
+        res.status(200).json(talker);
     } catch (err) {
         res.status(400).json({ message: req.message });
     }
